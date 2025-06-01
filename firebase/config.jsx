6da46841c59d7c58f3ca56fa7c7db19d5ecee10f
Giovanna de Moraes <gigimoraessol@gmail.com>
2025-06-01 10:28:18 -0300

    importing firebase

---
+++ firebase/config.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPUyfU3ajvr1um7SUbaX-WugzHgbL0yiQ",
+  authDomain: "minifeed-995ce.firebaseapp.com",
+  projectId: "minifeed-995ce",
+  storageBucket: "minifeed-995ce.firebasestorage.app",
+  messagingSenderId: "88020526383",
+  appId: "1:88020526383:web:458de13a661a8f32f46dda",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
