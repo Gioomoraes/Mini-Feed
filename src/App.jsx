e8fe8cd93cd14ceadb6acd6af3c0b47e3acfb221
Giovanna de Moraes <gigimoraessol@gmail.com>
2025-06-01 10:49:53 -0300

    Creating nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <div className="container">
-          <Routes></Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
         </div>
       </BrowserRouter>
     </div>
