b4e155e0dc338aa16632d9b8833d906a5b38d5e0
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-06-19 11:29:01 -0300

    importing hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 import {AuthProvider} from "./context/AuthContext";
 
