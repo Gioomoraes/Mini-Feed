a120ad0ecbce04a167f0781bd1aae77f308d189c
Giovanna de Moraes <gigimoraessol@gmail.com>
2025-06-22 11:08:04 -0300

    about page with basic css

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,20 @@
 import styles from "./About.module.css";
 
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Feed</span>
+      </h2>
+      <p>
+        Este projeto consiste em um feed feito com React no front-end e no
+        back-end o Firebase.
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar Post
+      </Link>
     </div>
   );
 };
