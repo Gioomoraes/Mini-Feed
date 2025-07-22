a053bbed4596016e1dc213ce11451b1ac296aa55
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-07-22 09:36:07 -0300

    login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,41 @@ import styles from "./Login.module.css";
 const Login = () => {
   return (
     <div>
-      <h2>Login</h2>
+      <h1>Entrar</h1>
+      <p>Crie seu usuário e compartilhe suas postagens!</p>
+      <form onSubmit={handleSubmit}>
+        
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do Usuário"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
+      </form>
     </div>
   );
 };
