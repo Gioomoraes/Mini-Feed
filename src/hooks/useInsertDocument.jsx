8ce503defe3a83680b60334f8dd91d8634767faa
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-07-24 19:58:14 -0300

    hook for posts

---
+++ src/hooks/useInsertDocument.jsx
@@ -0,0 +1,17 @@
+import {useState, useEffect, useReducer} from 'react';
+import {db} from '../firebase/config';
+import {collection, addDoc, Timestamp} from 'firebase/firestore';
+
+const initialState = {
+    loading: null,
+    error: null,
+};
+
+const insertReducer = (state, action) => {
+
+}
+
+export const useInsertDocument = (docCollection) => {
+    const [response, dispatch] = useReducer(insertReducer, initialState)
+
+}
