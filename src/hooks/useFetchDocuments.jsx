c46dfdc7c3e981bc346a1da10a9cd405d3bd45c7
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-08-02 20:53:56 -0300

    getting the data

--- src/hooks/useFetchDocuments.jsx
+++ src/hooks/useFetchDocuments.jsx
@@ -6,6 +6,7 @@ import {
   query,
   orderBy,
   where,
+  QuerySnapshot,
 } from "firebase/firestore";
 
 export const useFetchDocuments = (docCollection, search = null, uid = null) => {
@@ -14,4 +15,31 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
   const [loading, setLoading] = useState(null);
 
   const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      if (cancelled) return;
+
+      setLoading(true);
+
+      const collectionRef = await collection(db, docCollection);
+
+      try {
+        let q;
+
+        q = await query(collectionRef, orderBy("createdAt", "desc"));
+        await onSnapshot(q, (QuerySnapshot) => {
+          setDocuments(
+            querySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+        });
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+      }
+    }
+  }, [docCollection, search, uid, cancelled]);
 };
