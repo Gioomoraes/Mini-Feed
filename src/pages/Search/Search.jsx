9e360d585d90f6df0dd59888c1fb02a4c3bf09fe
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2025-08-03 11:23:40 -0300

    getting the q attribute

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -4,6 +4,9 @@ import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
 
 const Search = () => {
+  const query = useQuery();
+  const search = query.get("q");
+
   return (
     <div>
       <h2>Search</h2>
